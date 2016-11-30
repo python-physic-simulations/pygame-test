7cf900503bc0451f8a7f6159b986095bcb4c5881
mone27 <mone.massaro@gmail.com>
2016-11-30 19:20:49 +0100

    base physic obj class works!!

--- testing_vector.py
+++ testing_vector.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
-from sympy.vector import CoordSysCartesian
+from sympy.vector import CoordSysCartesian, Vector
+from sympy import Rational
 N = CoordSysCartesian('N')
-x0 = N.origin
-v0 = 3*N.i
-
-def update_pos(time):
-    global x0
-    x = N.origin.locate_new('p', x0.position_wrt(N) + v0 * time)
-    print(x.position_wrt(N))
-    x0 = x
- 
  
 class physic_obj() :
-    def __init__(self, p = N.origin, vel = Vector.Zero):
+    def __init__(self, p = N.origin, vel = Vector.zero, 
+                 mass = Rational(1,1),
+                 ref_frame = N ):
         self.p = p
         self.vel = vel
+        self.mass = mass
+        self.ref = ref_frame
+        self.forces = []
     def update_pos(self, t):
-        p = N.origin.locate_new('p', p.position_wrt(N) + vel * time)
+        tot_force = Vector.zero
+        for f in self.forces:
+            tot_force += f
+        acc = tot_force / self.mass
+        self.p = self.ref.origin.locate_new('p', self.p.position_wrt(self.ref) +\
+        self.vel * t + Rational(1,2) * acc * t ** 2)
+#        self.forces = []# not sure that is good idea..
+ball = physic_obj(N.origin, N.i + N.j, Rational(2,1))  
+ball.forces.append(N.i + N.j)
+        
